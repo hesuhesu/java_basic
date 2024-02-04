6643e7ec44cb34d99de419ba6190295dcc203042
hesuhesu <zndwk0728@gmail.com>
2024-02-04 21:17:00 +0900

    add controlStatement_forEach.java

---
+++ controlStatement/controlStatement_forEach.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class controlStatement_forEach {
+    public static void main (String[] args){
+
+        String[] numbers = {"one", "two", "three"};
+        for(String number: numbers) {
+            System.out.println(number); // python 문법처럼 해당 iterate에 대한 내용을 출력 가능함.
+        }
+
+        ArrayList<String> numbers2 = new ArrayList<>(Arrays.asList("one", "two", "three"));
+        for (String number : numbers2) {
+            System.out.println(number);
+        }
+    }
+}
